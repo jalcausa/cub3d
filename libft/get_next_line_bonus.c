84745cc374b663c1b95a25357bf4d829ae195a7f
yanel <yanelperaltarodriguez77@gmail.com>
2025-09-23 17:26:37 +0200

    solved errors for gnl

--- libft/get_next_line_bonus.c
+++ libft/get_next_line_bonus.c
@@ -5,125 +5,111 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yz <yz@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/17 12:39:21 by yaperalt          #+#    #+#             */
-/*   Updated: 2025/09/12 06:05:29 by yz               ###   ########.fr       */
+/*   Created: 2025/09/23 17:16:52 by yz                #+#    #+#             */
+/*   Updated: 2025/09/23 17:18:04 by yz               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
- $ This function takes a fd and the buffer used to store the content,
- $ appending its content until a newline('\n') is found.
- $ If there are no bytes to be read(read() returns -1), we free the pointer
- $ and return NULL.
- $ ft_strjoin is used to copy the content from the temp variable to the buffer.
- */
-
-char	*ft_read_fd(int fd, char *buffer)
+char	*ft_line(char *buffer)
 {
-	char	*temp;
-	int		bytes_count;
-
-	temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!temp)
-		return (NULL);
-	bytes_count = 1;
-	while (!ft_strchr(buffer, '\n') && (bytes_count != 0))
-	{
-		bytes_count = read(fd, temp, BUFFER_SIZE);
-		if (bytes_count < 0)
-		{
-			free(buffer);
-			free(temp);
-			return (NULL);
-		}
-		temp[bytes_count] = '\0';
-		buffer = ft_strjoin(buffer, temp);
-	}
-	free(temp);
-	return (buffer);
-}
-
-/*
- $ We allocate memory for the size of the buffer + 2, so we have space for '\n'
- $ if it's present and '\0' to end it.
- */
-char	*ft_get_actual_line(char *buffer)
-{
+	int		i;
 	char	*line;
-	size_t	i;
 
-	if (buffer[0] == '\0')
-		return (NULL);
 	i = 0;
+	if (!buffer[i])
+		return (NULL);
 	while (buffer[i] != '\0' && buffer[i] != '\n')
 		i++;
-	line = (char *)malloc(sizeof(char) * (i + 2));
-	if (!line)
-		return (NULL);
+	line = ft_calloc(i + 2, sizeof(char));
 	i = 0;
 	while (buffer[i] != '\0' && buffer[i] != '\n')
 	{
 		line[i] = buffer[i];
 		i++;
 	}
-	if (buffer[i] == '\n')
-	{
-		line[i] = buffer[i];
-		i += 1;
-	}
-	line[i] = '\0';
+	if (buffer[i] != '\0' && buffer[i] == '\n')
+		line[i] = '\n';
 	return (line);
 }
 
-char	*ft_remainder(char *buffer)
+/*This function serves to free the buffer of the previously read line, and keep
+only what could be stored of the next one, in order to later store that remainder
+in the static variable (here 'leftover') when it goes through get_next_line.*/
+
+char	*ft_next_line(char *buffer)
 {
-	char	*remainder;
-	size_t	i;
-	size_t	j;
+	int		i;
+	int		j;
+	char	*leftover;
 
 	i = 0;
-	if (!buffer)
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\0')
 	{
 		free(buffer);
 		return (NULL);
 	}
-	while (buffer[i] != '\0' && buffer[i] != '\n')
-		i++;
-	remainder = (char *)malloc(sizeof(char) * (ft_strlen(buffer) - i + 1));
-	if (!remainder)
-		return (NULL);
+	leftover = ft_calloc(ft_strlen(buffer) - i + 1, sizeof(char));
+	i++;
 	j = 0;
-	while (buffer[i])
-		remainder[j++] = buffer[++i];
-	remainder[j] = '\0';
+	while (buffer[i] != '\0')
+	{
+		leftover[j] = buffer[i];
+		i++;
+		j++;
+	}
 	free(buffer);
-	return (remainder);
+	return (leftover);
 }
 
-/*
- $	The default maximum number of file descriptors varies by system.
- $
- $	Linux (modern distributions):
- $	Default soft limit (ulimit -n): 1024
-*/
+/*This function replicates the functionality of read(), as it takes the value
+of BUFFER_SIZE to read the same amount of bytes.
+
+In addition, it is the one that concatenates the read lines, because we can not
+read the entire file at once. Therefore, we will be returning line by line and 
+maintaining the position of the 'cursor' thanks to the static variable.*/
+
+char	*ft_read_line(int fd, char *buffer)
+{
+	char	*tmp;
+	int		n;
+
+	tmp = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	n = 1;
+	while (n != 0 && !ft_strchr(buffer, '\n'))
+	{
+		n = read(fd, tmp, BUFFER_SIZE);
+		if (n == -1)
+		{
+			free(buffer);
+			free(tmp);
+			return (NULL);
+		}
+		tmp[n] = '\0';
+		buffer = ft_strjoin_sl(buffer, tmp);
+	}
+	free(tmp);
+	return (buffer);
+}
+
+/*This function actually doesn't do much, because the different actions are
+performed by the other three, but it gives execution order and adds some error
+checks.*/
+
 char	*get_next_line(int fd)
 {
-	static char	*buffer[1024];
+	static char	*buffer;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 1024)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer[fd] = ft_read_fd(fd, buffer[fd]);
-	if (!buffer[fd])
+	buffer = ft_read_line(fd, buffer);
+	if (!buffer)
 		return (NULL);
-	line = ft_get_actual_line(buffer[fd]);
-	buffer[fd] = ft_remainder(buffer[fd]);
-	if (!line)
-	{
-		free(buffer[fd]);
-		buffer[fd] = NULL;
-	}
+	line = ft_line(buffer);
+	buffer = ft_next_line(buffer);
 	return (line);
 }
